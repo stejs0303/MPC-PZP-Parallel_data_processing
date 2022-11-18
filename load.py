c65c9307939aaba3095f62a91e9657fc8bb73b2d
Jan Stejskal <janstej1@gmail.com>
2022-11-18 20:39:47 +0100

    Simple regex

--- load.py
+++ load.py
@@ -1,6 +1,13 @@
 import time
 import re
 
+def isnt_number(s):
+    try:
+        float(s)
+        return False
+    except ValueError:
+        return True
+
 def load(data_path: str, stop_words_path: str):
     
     data, stop_words = [], []
@@ -11,15 +18,20 @@ def load(data_path: str, stop_words_path: str):
 
             if(not len(line)): break
 
-            data.extend([word for word in re.split(" |,|;|\n|\r|\.|\\|\*|\+|\?|\[|\]|\(|\)|\{|\}|\!|\:|\-\-|\-\-\-|_|\'|\"", line) if word])
+            data.extend([str.lower(word) 
+                         for word in re.split(r"_|[\b\W\b]+", line)
+                         if (word and isnt_number(word))])
     
     with open(stop_words_path, 'r') as file_stop_words:
-        stop_words = [word.strip(" \n\r") for word in file_stop_words.readlines()]
+        stop_words = [str.lower(word.strip(" \n\r")) 
+                      for word in file_stop_words.readlines()]
     
     return data, stop_words
 
 if __name__ == "__main__":
     start = time.time_ns()
-    load("./files/data.txt", "./files/stop_words.txt")
+    data, stop_words = load("./files/data.txt", "./files/stop_words.txt")
     stop = time.time_ns()
-    print(f"{(stop-start)/int(1e6)} ms")
+    #print(f"{(stop-start)/int(1e6)} ms")
+    #print(data[:50])
+    #print(stop_words)
